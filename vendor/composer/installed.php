5ccc5f4d85d354ee3eef265945422c088453f7cf
SantamariaRamosJ <77521543+SantamariaRamosJ@users.noreply.github.com>
2021-04-14 20:05:21 -0400

    composer added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b012a5390ff38c711cc9151fbbe670aaf4a22516',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b012a5390ff38c711cc9151fbbe670aaf4a22516',
+    ),
+  ),
+);
