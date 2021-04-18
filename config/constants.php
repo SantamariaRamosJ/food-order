258bf05614eae1f7c6d0ecb38a01d2d4aec2fe4f
SantamariaRamosJ <77521543+SantamariaRamosJ@users.noreply.github.com>
2021-04-18 16:33:59 -0400

    constants added

--- config/constants.php
+++ config/constants.php
@@ -3,14 +3,14 @@
     session_start();
 
     //Create constants to store non repeating values
-    define('SITEURL', 'http://localhost/food-order/');
-    define('LOCALHOST', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'food-order');
+    define('SITEURL', 'https://food-order-app-php.herokuapp.com/');
+    define('LOCALHOST', 'sql10.freemysqlhosting.net');
+    define('DB_USERNAME', 'sql10406117');
+    define('DB_PASSWORD', 'JVKyKUPpfy');
+    define('DB_NAME', 'sql10406117');
 
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error("error")); //Database connection
-    $db_select = mysqli_select_db($conn, 'food-order') or die(mysqli_error("error")); //selecting database 
+    $db_select = mysqli_select_db($conn, 'sql10406117') or die(mysqli_error("error")); //selecting database 
 
 
 
