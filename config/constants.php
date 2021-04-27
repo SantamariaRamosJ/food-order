53fd2efafa2a87f7869b527dea7d56e798abc073
SantamariaRamosJ <77521543+SantamariaRamosJ@users.noreply.github.com>
2021-04-26 20:51:05 -0400

    db fixed

--- config/constants.php
+++ config/constants.php
@@ -2,15 +2,13 @@
     //Start Session
     session_start();
 
-    //Create constants to store non repeating values
     define('SITEURL', 'https://ohmyfood-app.herokuapp.com/');
     define('LOCALHOST', 'sql10.freemysqlhosting.net');
-    define('DB_USERNAME', 'sql10406117');
-    define('DB_PASSWORD', 'JVKyKUPpfy');
-    define('DB_NAME', 'sql10406117');
+    define('DB_USERNAME', 'sql10408524');
+    define('DB_PASSWORD', '51cFb5xmVR');
+    define('DB_NAME', 'sql10408524');
 
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error("error")); //Database connection
-    $db_select = mysqli_select_db($conn, 'sql10406117') or die(mysqli_error("error")); //selecting database 
-
+    $db_select = mysqli_select_db($conn, 'sql10408524') or die(mysqli_error("error")); //selecting database 
 
 ?>
--- partials-front/menu.php
+++ partials-front/menu.php
@@ -13,7 +13,7 @@ include('config/constants.php');
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <!--Fonts-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Tangerine&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Tangerine:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.min.js"></script>
     <title>OhMyFood - Food Delivery</title>
