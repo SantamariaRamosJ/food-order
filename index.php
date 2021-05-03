553f0688d7f285f098b868e763d0d547e8770f54
SantamariaRamosJ <77521543+SantamariaRamosJ@users.noreply.github.com>
2021-05-03 17:44:11 -0400

    new commit

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <div class="container">
             <form action="<?php echo SITEURL; ?>food-search.php" method="POST">
                 <input type="search" name="search" placeholder="Search for Food.." required>
-                <input type="submit" name="submit" value="Search" class="btn btn-primary">
+                <input type="submit" name="submit" value="Search" class="search-button">
             </form>
         </div>
     </section>
